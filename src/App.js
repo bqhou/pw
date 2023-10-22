bd84f26116b73a863f1458c050bade4c8cad3f37
bethhou <bingqihou@gmail.com>
2023-10-22 19:28:01 -0400

    class update

--- src/App.js
+++ src/App.js
@@ -1,25 +1,25 @@
-import logo from './logo.svg';
-import './App.css';
+import React, { useEffect } from 'react'
+import styles from './App.module.css'
+import axios from 'axios'
 
-function App() {
+const App = () => {
+
+    useEffect(()=>{
+        axios.get('https://yanchu.maoyan.com/api/mobile/comment/project/hotComment?projectId=233014&needUserComment=false&hotCommentLimit=10&optimus_risk_level=71&optimus_code=10&yodaReady=h5&csecplatform=4&csecversion=2.2.1').then(res=>{
+            console.group(res.data)
+        })
+
+    },[])
+    
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+    <div>
+        <ul>
+        <li className={styles.item}>hahhah</li>
+        <li>good</li>
+        </ul>
+      
+        </div>
+  )
 }
 
-export default App;
+export default App
